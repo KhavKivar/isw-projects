4256d08ae91bf8dcffe49f816c0274dea7b666d2
Hugo Leyton <hugo.leyton@sansano.usm.cl>
2020-07-21 22:23:28 -0400

    Initial Commit

---
+++ src/main/java/com/isw/nhr/NhrApplication.java
@@ -0,0 +1,13 @@
+package com.isw.nhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isw/nhr/NhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isw.nhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
