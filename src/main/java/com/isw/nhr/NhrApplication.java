3751c06678c70c54e30bd5b63998071469d26ba5
Sebastian Calderon <sebastian.calderon@sansano.usm.cl>
2020-07-22 02:58:03 -0400

    Se agrego la config de la bd

--- src/main/java/com/isw/nhr/NhrApplication.java
+++ src/main/java/com/isw/nhr/NhrApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NhrApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NhrApplication.class, args);
 	}
-
 }
