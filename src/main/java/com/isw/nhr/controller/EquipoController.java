3b88789e74a5ddaadd62f74fc251945ef206b51d
Sebastian Calderon <sebastian.calderon@sansano.usm.cl>
2020-07-24 10:48:16 -0400

    se corrigio los metodos

--- src/main/java/com/isw/nhr/controller/EquipoController.java
+++ src/main/java/com/isw/nhr/controller/EquipoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -109,7 +110,7 @@ public class EquipoController {
 	
 	
 	
-	@PostMapping("/edit")
+	@PutMapping("/edit")
 	public ResponseEntity<Equipo> EditEquipo(
 			@RequestParam("Id") List<String> Ids,@RequestParam("Name") String name,
 			@RequestParam("IdEquipo") String idEquipo
--- src/main/java/com/isw/nhr/controller/SolicitudController.java
+++ src/main/java/com/isw/nhr/controller/SolicitudController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -79,7 +80,7 @@ public class SolicitudController {
 
 	}
 	
-	@PostMapping("/all")
+	@GetMapping("/all")
 	public  @ResponseBody List<Solicitud> allSolicitudes(){
 		List<Solicitud> allSol = new ArrayList<>();
 		solicitudService.listAll().forEach((sol) -> 
